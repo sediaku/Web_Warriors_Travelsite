58aa2207ee891c79ed38c97410ebb38b022f842d
n-kirstyy <n.kirstyy@gmail.com>
2024-11-23 15:11:11 +0000

    Add files via upload
    Added signup and l

---
+++ navbar_guest.php
@@ -0,0 +1,17 @@
+<div class="topnav">
+    <h1>TRAVEL+ GH</h1>
+    <nav class="bar">
+        <a href="">Storyboard</a>
+        <a href="">Contact Us</a>
+        <a href="">About The Project</a>
+        <a href="index.php" class="active">Home</a>
+
+    </nav>
+    <div>
+        <button class = "view-more">
+            <span class="material-symbols-outlined">menu</span>
+        </button>
+    </div>
+</div>
+
+
