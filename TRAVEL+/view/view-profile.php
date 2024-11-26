39ba0a123c43ec4bc17bead834f94ca7ac59d925
n-kirstyy <n.kirstyy@gmail.com>
2024-11-26 18:11:47 +0000

    view profile

--- TRAVEL+/view/view-profile.php
+++ TRAVEL+/view/view-profile.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
-        <title>Blog Post</title>  
+        <title>Profile</title>  
         <link rel = "stylesheet" href = "../assets/css/style.css">
-        <link rel = "stylesheet" href = "../assets/css/blog-style.css">
+        <link rel = "stylesheet" href = "../assets/css/profile-style.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Symbols+Outlined" rel="stylesheet">
     </head>
 
@@ -17,8 +17,9 @@
         <section class="profile-view">
             <div class = "left">
                 <div id = "pfp">
-                    <button class = "edit-btns">Edit</button>
+                    
                 </div>
+                <button class = "edit-btns">Edit</button>
 
             </div>
 
